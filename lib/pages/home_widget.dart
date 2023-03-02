4925ac4a8d9cc3a1d3a0c3c458a282ecb972f7d6
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-02 12:05:03 +0100

    Called the bottom navigation bar widget

--- lib/pages/home_widget.dart
+++ lib/pages/home_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:bankly/bottom_navigation_bar.dart';
 import 'package:bankly/components/home/activity_component.dart';
 import '../components/home/header_component.dart';
 import '../components/home/operations_component.dart';
@@ -10,6 +11,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      bottomNavigationBar:  const NavigationAppBar(),
       body: ListView(
         children:   [
           const HomeHeader(),
