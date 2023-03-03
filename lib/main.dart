033ba91287cee9c0c731a108c14acdacb1ab8f7c
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-03 15:26:20 +0100

    Add the home , register routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'package:bankly/pages/login_widget.dart';
+import 'package:bankly/pages/register_widget.dart';
+import 'package:bankly/util/app_color.dart';
 import 'pages/home_widget.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(const MyApp());
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Roboto',
-        primaryColor: Colors.blueAccent,
-        scaffoldBackgroundColor: const Color(0xFFFAFAFA),
+        primaryColor: AppColor.darkBlue,
+        scaffoldBackgroundColor: AppColor.scaffoldBackColor,
       ),
       routes: {
-        '/' : (context) => const HomePage(),
+        '/' : (context) => const LoginWidget(),
+        'home' : (context) => const HomePage(),
+        'register' : (context) => const RegisterWidget(),
         //'profile' : (context) => Scaffold(),
-        //'logIn' : (context) => Scaffold(),
-        //'signUp' : (context) => Scaffold(),
         //'transaction' : (context) => Transaction()
       },
     );
