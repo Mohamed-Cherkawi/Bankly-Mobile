12ad1f513f7fab6bf8ea0de9bdd257ded453258a
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2023-03-05 15:41:46 +0100

    Calling the future builder method to behave upon the existence or not of the called data

--- lib/components/home/activity_component.dart
+++ lib/components/home/activity_component.dart
@@ -1,21 +1,11 @@
-import 'package:bankly/enums/operation_type.dart';
-import 'package:bankly/util/home_activity_component_data.dart';
+import 'package:bankly/service/transaction_service.dart';
+import 'package:bankly/util/TransactionsList.dart';
+import 'package:bankly/model/transaction_maper.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 class ActivityWidget extends StatelessWidget {
-  final List<HomeActivityUtil> activities = [
-    HomeActivityUtil('Anass Choubri', '2 hours ago', 250.00, OperationType.received),
-    HomeActivityUtil('Ossama Souyari', 'yesterday', 138.50, OperationType.sent),
-    HomeActivityUtil('Mohamed Maitite', '1 week ago', 531.00, OperationType.received),
-    HomeActivityUtil('Salima Foulad', '10 week ago', 31.00, OperationType.received),
-    HomeActivityUtil('Firdaouss Cherkaoui', '1 year ago', 10.00, OperationType.sent),
-    HomeActivityUtil('Firdaouss Cherkaoui', '1 year ago', 10.00, OperationType.sent),
-    HomeActivityUtil('Firdaouss Cherkaoui', '1 year ago', 10.00, OperationType.sent),
-    HomeActivityUtil('Firdaouss Cherkaoui', '1 year ago', 10.00, OperationType.sent)
-  ];
-
-  ActivityWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -42,69 +32,17 @@ class ActivityWidget extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 10),
-          for(HomeActivityUtil activity in activities)
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            decoration:  BoxDecoration(
-              color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 4,
-                    offset: Offset(0,3)
-                  )
-                ]
-            ),
-            width: 400,
-            height: 80,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'images/user-account.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 5),
-                      width: 230,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children:  [
-                          Text(
-                            activity.accountHolderName,
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                              activity.date,
-                              style: const TextStyle(
-                                fontSize: 10
-                              ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                (activity.operationType == OperationType.received)
-                    ?
-                   Text(
-                     '+\$${activity.amount.toString()}',
-                     style: const TextStyle(color: Colors.green),
-                   )
-                   :
-                Text(
-                  '-\$${activity.amount.toString()}',
-                  style: const TextStyle(color: Colors.red),
-                )
-              ],
-            ),
+          FutureBuilder<List<Transaction>>(
+            future: TransactionService.fetchTransactions(http.Client()),
+            builder: (context,snapshot){
+              if (snapshot.hasData) {
+                return TransactionsList(transactions: snapshot.data!);
+              } else if (snapshot.hasError) {
+                return Text('No Activity To Show');
+              }
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            }
           ),
         ],
       ),
